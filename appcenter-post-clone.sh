a4c3e0c80d0115b5f5502b141e338011b26d5a47
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-01-21 21:05:06 -0800

    add post clone sh

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp habitica.properties.example habitica.properties
+cp habitica.resources.example habitica.resources
+cp Habitica/google-services.json.example Habitica/google-services.json
