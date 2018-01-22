b6a3553a6762aaccb3404424d0326f15c3aa4e51
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-01-21 22:06:41 -0800

    Revert "add post clone sh"
    This reverts commit a4c3e0c80d0115b5f5502b141e338011b26d5a47.

--- appcenter-post-clone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cp habitica.properties.example habitica.properties
-cp habitica.resources.example habitica.resources
-cp Habitica/google-services.json.example Habitica/google-services.json
